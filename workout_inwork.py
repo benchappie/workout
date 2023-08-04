e5dcc0b3ebf23c84afbcc007b4b5c86c58ca9c27
Benjamin Chapman <benjaminchapman@Benjamins-MacBook-Air.local>
2023-08-03 22:38:00 -0400

    updated

--- workout_inwork.py
+++ workout_inwork.py
@@ -1,31 +1,30 @@
-"""Get data about workouts from users, read/write to csv"""
+"""Get data about workouts from users, read/write to DB. 'Sets' are fundamental unit of input"""
 
 import pandas as pd
 import mysql.connector
+from getpass import getpass
+import numpy as np
 
 #initialize the LISTS needed for user inputs
 dates = []
-names = [] 
-sets = [] 
+exercises = [] 
 reps = [] 
 weights = [] 
 
-#initialize class to include date/name/sets/reps/weight attributes
-class Exercise:
+#initialize class to include date/name/reps/weight attributes
+class Set:
     counter = 0
 
-    def __init__(self, date, name, sets, reps, weight):
+    def __init__(self, date, exercise, reps, weight):
         self.date = date
-        self.name = name
-        self.sets = sets
+        self.exercise = exercise
         self.reps = reps
         self.weight = weight
 
     #method to offload data from class to lists
-    def recordExercise(self):
+    def recordSet(self):
         dates.append(self.date)
-        names.append(self.name)
-        sets.append(self.sets)
+        exercises.append(self.exercise)
         reps.append(self.reps)
         weights.append(self.weight)
 
@@ -34,24 +33,45 @@ addExercise = "Y"
 
 while addExercise == "Y":
 
-    exercise = Exercise(input("Date: "), input("Exercise: "), input("Sets: "), input("Reps: "), input("Weight: "))
-    Exercise.recordExercise(exercise)
-    Exercise.counter += 1
+    set = Set(input("Date: "), input("Exercise: "), input("Reps: "), input("Weight: "))
+    Set.recordSet(set)
+    Set.counter += 1
 
     #provide offramp from loop
-    addExercise = input("\nEnter Y if you have another exercise, N if you do not: ")
-    if addExercise == "Y":
+    addSet = input("\nEnter Y if you have another set, N if you do not: ")
+    if addSet == "Y":
         continue
     else:
         break
 
-#make the lists into a dictionary and make a dataframe
-fitness = {'date': dates, 'name': names, 'sets': sets, 'reps': reps, 'weight': weights}
-df = pd.read_csv("fitness_frame.csv", usecols=[1,2,3,4,5])
-df = df.append(pd.DataFrame(fitness))
+#use the zip function to create a list of tuples
+zipped_sets = list(zip(dates, exercises, reps, weights))
 
-#print for viewing purposes
-print(df)
+#create connection with DB and set cursor to allow python to talk with DB
+mydb = mysql.connector.connect(
+    host="localhost",
+    user=input("Enter username: "),
+    password=getpass("Enter password: "),
+    database="workoutdb2",
+) 
 
-#push df to csv
-df.to_csv("fitness_frame.csv")
+cursor = mydb.cursor()
+
+#using a sql formula, enter the following SQL query, for each line of inputting the zipped values
+insert_set = "INSERT INTO sets (date, exercise, reps, weight) VALUES (%s, %s, %s, %s)"
+
+for values in zipped_sets:
+    cursor.execute(insert_set, values)
+
+mydb.commit()
+
+#print what is currently in the table for user to view
+cursor.execute("SELECT * FROM sets")
+result = cursor.fetchall()
+
+for row in result:
+    print(row)
+
+cursor.close()
+mydb.close()
+
